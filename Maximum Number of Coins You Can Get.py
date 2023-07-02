98b9c2b341a70da66cf4b7573832f1957a92eb84
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-07-02 23:05:16 +0300

    Update Maximum Number of Coins You Can Get.py

--- Maximum Number of Coins You Can Get.py
+++ Maximum Number of Coins You Can Get.py
@@ -1,10 +1,12 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
+        piles.sort(reverse=True)
         re=0
-        piles.sort()
-        while piles:
-            piles.pop()
-            re+=piles.pop()
-            piles.pop(0)
+        i=0
+        while i<len(piles):
+            a=piles[i]
+            re+=piles[i+1]
+            b=piles.pop()
+            i+=2
         return re
 
