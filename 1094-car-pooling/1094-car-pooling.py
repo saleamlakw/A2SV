c0def7d19e662907f8072edb5216f0dd7ed1ffa5
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-08-02 14:32:09 +0300

    Time: 64 ms (99.91%), Space: 17.1 MB (10.81%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.py
@@ -0,0 +1,40 @@
+class Solution:
+    from collections import defaultdict
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key=lambda x: (x[1]))
+        if capacity>=trips[0][0]:
+            capacity-=trips[0][0]
+            tri=defaultdict(list)
+            tri[trips[0][0]].append(trips[0][2])
+            print("tri",tri)
+            # print("c",capacity)
+        else:
+            return False
+        for k,a,b in trips[1:]:
+            if capacity>=k:
+                capacity-=k
+                tri[k].append(b)
+                print("c",capacity)
+            else:
+                k-=capacity
+                r=0
+                po=[]
+                for j in tri:
+                    # print("j",j)
+                    # print("k",k)
+                    tri[j].sort(reverse=True)
+                    while tri[j] and tri[j][-1]<=a:
+                        r+=j
+                        tri[j].pop()
+                if r>=k: 
+                    tri[k+capacity].append(b)
+                    capacity=r-k
+                else:
+                    return False
+        return True
+                        
+                
+            
+            
+            
+        
