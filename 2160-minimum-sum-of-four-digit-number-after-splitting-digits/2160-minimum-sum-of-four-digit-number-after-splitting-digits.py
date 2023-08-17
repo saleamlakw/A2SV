08cb3fb75caa718c3c159c3caa6f278323100f89
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-08-17 22:23:33 +0300

    Time: 41 ms (67.08%), Space: 16.4 MB (23.82%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
@@ -1,11 +1,7 @@
 class Solution:
     def minimumSum(self, num: int) -> int:
         a=list(str(num))
-        a=list(map(int,a))
-        a.sort()
-        re=[]
+        a=sorted(list(map(int,a)))
         x=int(str(a[0])+str(a[2]))
-        print(x)
         y=int(str(a[1])+str(a[3]))
-        print(y)
         return x+y
