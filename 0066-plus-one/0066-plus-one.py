5bea851cbdfe88cce66d53faca82133592018261
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-08-17 15:30:01 +0300

    Time: 35 ms (94.39%), Space: 16.2 MB (66.84%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,8 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits=list(map(lambda x:str(x),digits))
+        re="".join(digits)
+        re=int(re)+1
+        return [int(i) for i in str(re)]
+        
+        
