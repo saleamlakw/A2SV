dcece4c188ce1b6294a218dc3a34e0b6206afc56
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-08-17 14:57:34 +0300

    Time: 46 ms (62.26%), Space: 16.5 MB (32.97%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sim(self,left,right):
+        re=""
+        i=0
+        while i<len(left) and i<len(right):
+            if left[i]==right[i]:
+                re+=left[i]
+            else:
+                return re
+            i+=1
+        return re
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        left=strs[0]
+        for i in range(1,len(strs)):
+            right=strs[i]
+            print(left)
+            print(right)
+            left=self.sim(left,right)
+            if not left:
+                return ""
+        return left
+        
