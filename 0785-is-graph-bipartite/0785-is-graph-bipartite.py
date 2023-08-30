a2e673662421bcf7eef5f87b7d9dad31cebb63bf
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-08-30 17:30:31 +0300

    Time: 168 ms (58.83%), Space: 16.8 MB (51.09%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,27 +1,25 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        visited=[]
+        visited=set()
         for u in range(len(graph)):
             if graph[u] and u not in visited:
-                        stack=[u]
-                        visited=set([u])
-                        color=[-1]*len(graph)
-                        if color[u]==-1:color[u]=0 
-                        while stack:
-                                node=stack.pop()
-                                for n in graph[node]:
-                                    if color[n]==color[node]:
-                                        return False
-                                    if color[n]==-1:
-                                        if color[node]==1:
-                                            color[n]=0
-                                        else:
-                                            color[n]=1
-                                    if n not in visited:
-                                        stack.append(n)
-                                        visited.add(n)
-                                    # print(color)
-                                print(color)
+                stack=[u]
+                visited=set([u])
+                color=[-1]*len(graph)
+                if color[u]==-1:color[u]=0 
+                while stack:
+                        node=stack.pop()
+                        for n in graph[node]:
+                            if color[n]==color[node]:
+                                return False
+                            if color[n]==-1:
+                                if color[node]==1:
+                                    color[n]=0
+                                else:
+                                    color[n]=1
+                            if n not in visited:
+                                stack.append(n)
+                                visited.add(n)
         return True
             
                         
