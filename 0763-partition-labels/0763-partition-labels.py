9a599f60d5bfe30940a1ee7a8ce2de9188d08921
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-09-10 23:23:57 -0700

    Time: 48 ms (52.68%), Space: 16.4 MB (32.33%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,24 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        visit=set()
+        co=Counter(s)
+        l=0
+        re=0
+        result=[]
+        for r in range(len(s)):
+            if s[r] not in visit:
+                re+=(co[s[r]]-1)
+                visit.add(s[r])
+            else:
+                re-=1
+            if re==0:
+                print("l",l)
+                print("r",r)
+                result.append(r-l+1)
+                l=r+1
+        return result
+            
+                
+                
+                
+            
