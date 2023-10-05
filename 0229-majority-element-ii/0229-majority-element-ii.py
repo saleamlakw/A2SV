f66cb84f5587145ccc1e7f46d1ac7209f1445a84
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-10-05 17:40:03 +0300

    Time: 112 ms (51.76%), Space: 17.8 MB (49.91%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,9 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         counterDict=Counter(nums)
-        re=[]
-        for i in counterDict:
-            if counterDict[i]>len(nums)/3:
-                re.append(i)
-        return re
+        return [i for i in counterDict if counterDict[i]>len(nums)/3]
             
