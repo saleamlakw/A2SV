b34ad1e7ecbaba13d09ee46373a9093a2c9ea956
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-10-09 12:28:40 +0300

    Time: 87 ms (37.82%), Space: 17.6 MB (84.76%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,9 +1,26 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        mi=float('inf')
+        mi=float("inf")
         ma=float('-inf')
-        for i in range(len(nums)):
-            if nums[i]==target:
-                mi=min(mi,i)
-                ma=max(ma,i)
-        return [mi,ma] if mi!=float('inf') and ma!=float('inf') else [-1,-1]
+        l,r=0,len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if target==nums[mid]:
+                mi=min(mi,mid)
+                r=mid-1
+            elif target>nums[mid]:
+                l=mid+1
+            else:
+                r=mid-1
+        l,r=0,len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if target==nums[mid]:
+                ma=max(ma,mid)
+                l=mid+1
+            elif target>nums[mid]:
+                l=mid+1
+            else:
+                r=mid-1
+        return [mi,ma] if ma != float('-inf') and mi!=float("inf") else [-1,-1]
+            
