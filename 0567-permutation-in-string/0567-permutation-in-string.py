4fe4907ea42f574594769f575148042328dfa0ea
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-11-01 17:53:57 +0300

    Time: 2396 ms (15.31%), Space: 16.4 MB (62.32%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,21 +1,20 @@
 class Solution:
-    def check(self,left ,right):
-        l=Counter(left)
-        r=Counter(right)
-        for i in left:
-            if i not in r or l[i]!=r[i]:
-                return False
-        return True
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        def check(left,right):
+            leftCount=Counter(left)
+            rightCount=Counter(right)
+            for i in leftCount:
+                if leftCount[i]!=rightCount[i]:
+                    return False
+            return True
         window=deque()
-        l=0
-        k=len(s1)
-        for r in range(len(s2)):
-            window.append(s2[r])
-            while r-l+1>k:
+        for r in s2:
+            window.append(r)
+            while len(window)>len(s1):
                 window.popleft()
-                l+=1
-            if r-l+1==k:
-                if self.check(s1,s2[l:r+1]):
+            # print(window)
+            if len(window)==len(s1) :
+                if check(s1,window):
                     return True
-        return False
+        return  False
+                
