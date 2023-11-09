abc37ca41b14a5f4c602217b9a0452a51821573a
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-11-09 16:40:07 +0000

    Time: 235 ms (5.26%), Space: 14.4 MB (92.98%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.py
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.py
@@ -7,11 +7,8 @@ class Solution(object):
             window.append(r)
             while window and window[0]!=r:
                 window.popleft()
-            l,r=0,len(window)
             if len(window)>1:
-                        result+=len(window)-2
-            if len(window)>1:
-                    result+=1
+                        result+=len(window)-1
         return (result+len(s))%(10**9+7)
 # window=[a,a]
 # co={a:2,b:1,bb:1,c:1,cc:1,ccc:1,aa:1}
