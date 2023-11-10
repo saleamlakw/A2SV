2c23bc7703a7d8e59fc487f57208923066ba1831
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-11-10 16:08:15 +0000

    Time: 2792 ms (39.25%), Space: 55.1 MB (77.15%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -3,13 +3,11 @@ class Solution:
             arr=[1]*n
             if n<2:
                 return 0
-            # print(arr)
             arr[0]=arr[1]=0
             for i in range(2,n):
                 if arr[i] and i*i<n:
                     for j in range(i*i,n,i):
                         arr[j]=0
-                    # print(arr)
             result=0
             for k in arr:
                 if k:
