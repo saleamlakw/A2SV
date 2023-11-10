531eb1e1e4a6d3c338e4c25b06b6ff27896009a7
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-11-10 14:56:57 +0000

    Time: 42 ms (29.99%), Space: 16.1 MB (74.40%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -4,14 +4,10 @@ class Solution:
         l,r=0,0
         switch=True
         while l<len(word1) and r<len(word2):
-            if switch:
                 result+=word1[l]
                 l+=1
-                switch=False
-            else:
                 result+=word2[r]
                 r+=1
-                switch=True
         if l<len(word1):
             result+=word1[l:]
         if r<len(word2):
