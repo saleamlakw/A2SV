665f0bb2dbdb6cb0163d4bc3509d0a173f0906e2
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-11-15 14:44:45 +0000

    Time: 147 ms (31.91%), Space: 17.8 MB (10.26%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,10 +1,24 @@
-from itertools import accumulate
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        arr1=list(accumulate(nums))
-        arr2=list(accumulate(nums[::-1]))[::-1]
-        for i in range(len(arr1)):
-            if arr1[i]==arr2[i]:
-                return i
+        prefix=[]
+        accumulator=0
+        for num in nums:
+            accumulator+=num
+            prefix.append(accumulator)
+        # print(prefix)
+        postfix=deque()
+        accumulator=0
+        for num in nums[::-1]:
+            accumulator+=num
+            postfix.appendleft(accumulator)
+        # print(postfix)  
+        for n in range(len(prefix)):
+            if prefix[n]==postfix[n]:
+                return n
         return -1
         
+"""
+i will calculate postfix sum of the array and prefix sum the array
+i will itertate through both array and find the index in which value in both array is equal
+i will use the index to find the the pivot element 
+"""
