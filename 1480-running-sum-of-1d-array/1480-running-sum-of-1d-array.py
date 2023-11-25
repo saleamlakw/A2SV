16e2817b28f392579225664c2b1252b0f1373f0b
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-11-25 07:40:12 +0000

    Time: 26 ms (23.06%), Space: 13.5 MB (34.28%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,12 +1,8 @@
 class Solution(object):
     def runningSum(self, nums):
+        accumulator=0
         prefixSum=[]
         for num in nums:
-            if prefixSum:
-                prefixSum.append(prefixSum[-1]+num)
-            else:
-                prefixSum.append(num)
+            accumulator+=num
+            prefixSum.append(accumulator)
         return prefixSum
-#intialize an array to keep track of the cummulative sum
-#i will iterate through nums array and append the cumulative sum upto that element and the element itsef into the prefixsum array
-#return the prefixsum array
