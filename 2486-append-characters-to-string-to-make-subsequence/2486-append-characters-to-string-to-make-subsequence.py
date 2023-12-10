9e47fc2d3cbe1a9e656078f4717e3cc20d424d10
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-12-10 08:15:34 +0000

    Time: 90 ms (35.24%), Space: 17.4 MB (79.24%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.py
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.py
@@ -1,35 +1,10 @@
 class Solution(object):
     def appendCharacters(self, s, t):
-        hashmap=Counter(t)
-        indhash=defaultdict(deque)
-        for i in range(len(s)):
-            if s[i] in hashmap:
-                indhash[s[i]].append(i)
-        # print(dict(indhash))
-        def check(c,r,h):
-            # nonlocal ll 
-            # print(ll,"ll")
-            if c in h:
-                while h[c]:
-                    if h[c][0]>r:
-                      
-                        return [True,h[c][0]]
-                    else:
-                        h[c].popleft()
-                    
-                        
-            return []
-        
-        ch=len(t)
-        r=0
-        while r < len(s):
-            if s[r]==t[0]:
-                l=1
-                ch=min(ch,len(t)-l)
-                while r < len(s) and l<len(t) and  (u := check(t[l],r,indhash)):
-                    ch=min(ch,len(t)-l-1)
-                    r=u[1]
-                    # print(r,"r")
-                    l+=1
-            r+=1
-        return ch
+        i=j=0
+        while i<len(s) and j<len(t):
+            if s[i]==t[j]:
+                i+=1
+                j+=1
+            else:
+                i+=1
+        return len(t)-j
