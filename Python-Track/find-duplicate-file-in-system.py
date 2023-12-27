151ec312b5a23dc5a1bf4fdc0151ec97f7d72220
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-12-27 03:55:45 +0000

    Added solution for Find Duplicate File in System

---
+++ Python-Track/find-duplicate-file-in-system.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        hashmap=defaultdict(list)
+        for e in paths:
+            all=e.split()
+            root=all[0]
+            for f in all[1:]:
+                l=f.split(".txt")
+                print(l)
+                num=l[0]
+                print(num)
+                content=l[1]
+                re=root+"/"+num+".txt"
+                hashmap[content].append(re)
+        return [hashmap[j] for j in hashmap if len(hashmap[j])>1]
