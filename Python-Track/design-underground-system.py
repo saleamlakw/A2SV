e95a9c43612bd90abc14183bf9a963c2a26fa9fb
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2023-12-31 13:13:21 +0000

    Added solution for Design Underground System

--- Python-Track/design-underground-system.py
+++ Python-Track/design-underground-system.py
@@ -1,19 +1,18 @@
 class UndergroundSystem:
 
     def __init__(self):
-        self.i=defaultdict(tuple)
-        self.o=defaultdict(list)
+        self.data=defaultdict(list)
+        self.hashmap=defaultdict(list)
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.i[id]=(t,stationName)
+        self.hashmap[id]=[stationName,t]
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        starttime,startstation=self.i[id]
-        total=t-starttime
-        self.o[(startstation,stationName)].append(total)
+        self.data[(self.hashmap[id][0],stationName)].append(t-self.hashmap[id][1])
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        return sum(self.o[(startStation,endStation)])/len(self.o[(startStation,endStation)])
+        # print(self.data)
+        return sum(self.data[(startStation,endStation)])/len(self.data[(startStation,endStation)])
         
 
 
