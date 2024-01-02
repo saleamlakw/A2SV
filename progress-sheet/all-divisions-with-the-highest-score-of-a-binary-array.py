8029e8c8d37d11a4498cc240478b942a01a3d0b9
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-01-02 12:05:17 +0000

    Added solution for All Divisions With the Highest Score of a Binary Array

--- progress-sheet/all-divisions-with-the-highest-score-of-a-binary-array.py
+++ progress-sheet/all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -4,11 +4,12 @@ class Solution:
         zero=0
         hashmap=defaultdict(list)
         hashmap[one+zero].append(0)
+        k=(one+zero)
         for i in range(len(nums)):
             if nums[i]==0:
                 zero+=1
             else:
                 one-=1
+            k=max(k,one+zero)
             hashmap[one+zero].append(i+1)
-        k=max(hashmap.keys())
         return hashmap[k]
