6811dbb55e06a84e49d8c503ca2d0350fe5cd3f3
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-04-10 09:50:02 +0300

    Time: 55 ms (29.55%), Space: 16.6 MB (25.81%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,25 +1,27 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        row,column=len(grid),len(grid[0])
-        q=deque()
-        time,fresh=0,0
-        for r1 in range(row):
-            for c1 in range(column):
-                if grid[r1][c1]==1:
+        queue=deque()
+        fresh=0
+        time=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==1:
                     fresh+=1
-                if grid[r1][c1]==2:
-                    q.append((r1,c1))
-        dire=[[0,1],[1,0],[-1,0],[0,-1]]
-        while q and fresh>0:
-            for i in range(len(q)):
-                r,c=q.popleft()
-                for dr,dc in dire:
-                    if min(r+dr,c+dc)<0 or r+dr==row or c+dc==column or grid[r+dr][c+dc]!=1:
-                        continue
-                    fresh-=1
-                    grid[r+dr][c+dc]=2
-                    q.append((r+dr,c+dc))
-            time+=1
-        return time if fresh==0 else -1 
-                
+                if grid[i][j]==2:
+                    queue.append([i,j])
         
+        directions=[[0,1], [1,0], [0,-1], [-1,0]]
+        def inbound(r,c):
+            return 0<=r<len(grid) and 0<=c<len(grid[0])
+        while queue and fresh>0:
+            for _ in range(len(queue)):
+                r,c=queue.popleft()
+                for rc,cc in directions:
+                    nr=r+rc
+                    nc=c+cc
+                    if inbound(nr,nc) and grid[nr][nc]==1:
+                        fresh-=1
+                        grid[nr][nc]=2
+                        queue.append([nr,nc])
+            time+=1
+        return time if fresh==0 else -1
