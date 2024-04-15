7e1a188fb182f0649f09e169e381767dfa27540e
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-04-15 15:56:40 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.py
@@ -0,0 +1,28 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        graph=defaultdict(list)
+        for emp in employees:
+            graph[emp.id]=[emp.importance,emp.subordinates]
+        print(graph)
+        visited=set()
+
+        def dfs(node):
+            visited.add(node)
+            if len(graph[node][1])==0:
+                return graph[node][0]
+            re=0
+            for nb in graph[node][1]:
+                if nb not in visited:
+                    re+=dfs(nb)
+            return graph[id][0]+ re
+        
+        return dfs(id) 
