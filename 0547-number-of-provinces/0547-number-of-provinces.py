a7ff2b74c212c3ba57a6fd5a0a098273b24634e3
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-04-24 18:26:05 +0300

    Time: 185 ms (57%), Space: 17.5 MB (45.16%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,21 +1,29 @@
+           
 class Solution:
+    
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def inbound(r,c):
-            return 0<=r<len(isConnected) and 0<=c<len(isConnected[0])
-        directions =[[0,1],[0,-1],[1,0],[-1,0]]
-
-        def dfs(r,c):
-            isConnected[r][c]=0
-            for rc,cc in directions:
-                nr=r+rc
-                nc=c+cc
-                if inbound(nr,nc) and isConnected[nr][nc]==1:
-                    dfs(nr,nc)
-        
-        result=0
+        root = [i for i in range(len(isConnected))]
+        size = [1] * len(isConnected)
+        def find(x):
+            if x == root[x]:
+                return x
+            root[x] =find(root[x])
+            return root[x]
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                if size[rootX] > size[rootY]:
+                    root[rootY] = rootX
+                    size[rootX] += size[rootY]
+                else:
+                    root[rootX] = rootY
         for i in range(len(isConnected)):
             for j in range(len(isConnected[0])):
                 if isConnected[i][j]==1:
-                    result+=1
-                    dfs(i,j)
-        return result
+                    union(i,j)
+        re=set()
+        for ele in range(len(isConnected)):
+            re.add(find(ele))
+        return len(re)
+
