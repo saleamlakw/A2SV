9379d5900e49273dbd0e5970ff1d23cbc37cda38
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-04-25 17:43:36 +0300

    Time: 1844 ms (5%), Space: 20.4 MB (58.8%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,43 +1,49 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        cc=Counter()
+        parent = {}
         g=defaultdict(set)
-        for ele in accounts:
-            name,email=ele[0],set(ele[1:])
-            ch=False
-            num=0
-            for ss in g:
-                rename=""
-                digit=""
-                for n in ss:
-                    if not n.isdigit():
-                        rename+=n
-                    else:
-                        digit+=n
-                digit=digit[::-1]
-                # print(rename,digit)
-                if rename==name:
-                    num=max(num,int(digit))
-                    if len(email.intersection(g[ss]))>0:
-                        # print("union",email.union(g[ss]))
-                        # print(email,g[ss])
-                        g[ss]=email.union(g[ss])
-                        ch=True
-                        break
-                    
-            if not ch:
-                # print("--",name,num,email)
-                g[name+str(num+1)]=email
-        #     print(g)
+        for acc in accounts:
+            parent[acc[0]+str(cc[acc[0]])]=acc[0]+str(cc[acc[0]])
+            g[acc[0]+str(cc[acc[0]])]=set(acc[1:])
+            cc[acc[0]]+=1
+        # print(parent)
         # print(g)
+        def find(x):
+            return parent[x]
+        def union(x, y):
+            parentX = find(x)
+            parentY = find(y)
+            # print("++++",parentX,parentY)
+            if parentX != parentY:
+                for node in parent:
+                    if parent[node] == parentX:
+                        parent[node] = parentY
+                # print("---",parentX,parentY,parent)
 
-        result=[]
-        for res in g:
-            rt=""
-            for l in res:
-                    if not l.isdigit():
-                            rt+=l
-            appe=[rt,*list(sorted(g[res]))]
-            result.append(appe)
-        return result
+        # print(g)
+        for ele1 in g:
+            for ele2 in g:
+                # print(ele1,ele2,len(g[ele1].intersection(g[ele2]))>=1 and  ele1!=ele2,g[ele1],g[ele2])
+                if len(g[ele1].intersection(g[ele2]))>=1 and  ele1!=ele2:
+                    union(ele1,ele2)
+        
+        # print(parent)
+        result=defaultdict(list)
 
+        for elef in g:
+            par=find(elef)
+            # print(elef,par)
+            result[par]=list(result[par])+list(g[elef])
+        # print(result)
+        rc=[]
+        for cv in result:
+            nameonly=""
+            for com in cv:
+                if not com.isdigit():
+                     nameonly+=com
+            oup=[nameonly,*list(sorted(list(set(result[cv]))))]
+            rc.append(oup)
+        return (rc)
+        
                    
