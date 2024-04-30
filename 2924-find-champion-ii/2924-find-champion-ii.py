47539283c61146c9acd1584a921f06b8e61c223a
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-04-30 07:01:40 +0300

    Time: 765 ms (44.97%), Space: 18.1 MB (16.67%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.py
+++ 2924-find-champion-ii/2924-find-champion-ii.py
@@ -11,22 +11,6 @@ class Solution:
         if indegree.count(0)!=1:
             return -1
 
-        def dfs(node):
-            nonlocal num_nodes
-            visited.add(node)
-            num_nodes+=1
-            for nb in graph[node]:
-                if  nb not in visited:
-                    dfs(nb)
-        
-        for i in range(len(indegree)):
-            if indegree[i]==0:
-                visited=set()
-                num_nodes=0
-                dfs(i)
-                # print(num_nodes)
-                if num_nodes==(n):
-                    return i
-        return -1
+        return indegree.index(0)
         
             
