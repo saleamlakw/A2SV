0b87bb3d52e2402e59a20992c064f116458faa91
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-04-30 07:35:36 +0300

    Time: 526 ms (33.29%), Space: 59.1 MB (8.65%) - LeetHub

---
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
@@ -0,0 +1,27 @@
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        graph=defaultdict(list)
+        for u,v in pairs:
+            graph[u].append(v)
+            graph[v].append(u)
+        def dfs(node):
+            visited.add(node)
+            temp.append(s[node])
+            ind.append(node)
+            for nb in graph[node]:
+                if nb not in visited:
+                    dfs(nb)
+        # print(graph)
+        visited=set()
+        re=[0]*len(s)
+        for i in range(len(s)):
+            if i not in visited:
+                temp=[]
+                ind=[]
+                dfs(i)
+                temp.sort()
+                ind.sort()
+                for k in range(len(ind)):
+                    re[ind[k]]=temp[k]
+        return ("".join(re))
+
