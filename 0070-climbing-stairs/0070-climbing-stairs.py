a84d729f4ba044e6cdb38cd9862ff2e229ffbad5
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-09 18:12:14 +0300

    Time: 41 ms (13.03%), Space: 16.7 MB (5.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo=Counter()
+        @lru_cache()
         def fib(n):
             if n<=0:
                 return 1
             re=0
             for i in range(1,3):
                 if n-i>=0:
-                    if n-i not in memo:
-                        memo[n-i]=fib(n-i)
-                    re+=memo[n-i]
+                    re+=fib(n-i)
             return re
         return fib(n)
