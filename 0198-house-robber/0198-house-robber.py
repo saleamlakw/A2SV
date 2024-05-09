06596cbb6794854245d3ff42c31115e333e1759c
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-09 19:31:08 +0300

    Time: 39 ms (33.25%), Space: 16.8 MB (12.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,6 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @lru_cache(maxsize=len(nums))
+        memo=Counter()
         def fn(n):
             if n==(len(nums)-1):
                 return nums[n]
@@ -8,8 +8,10 @@ class Solution:
                 return 0
             re=float("-inf")
             for i in range(2,4):
-                re=max(re,fn(n+i))
-            print(re,n)
+                if n+i not in memo:
+                    memo[n+i]=fn(n+i)
+                re=max(re,memo[n+i])
+            # print(re,n)
             return re+nums[n]
         return max(fn(0),fn(1)) if len(nums)>1 else fn(0)
        
