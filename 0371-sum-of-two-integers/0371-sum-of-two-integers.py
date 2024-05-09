aa5d36d1b40b81f9cf9fef347e96c49d6b1aed12
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-09 23:24:24 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,49 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        def addBinary(a,b):
+            car="0"
+            l=len(a)-1
+            r=len(b)-1
+            re=""
+            while l>=0 and r>=0:
+                if a[l]=="1" and b[r]=="1":
+                    re+=car
+                    car="1"
+                elif a[l]=="0" and  b[r]=="0":
+                    re+=car
+                    car="0"
+                else:
+                    if car=="1":
+                        re+="0"
+                        car="1"
+                    else:
+                        re+="1"
+                        car="0"
+                l-=1
+                r-=1
+            while l>=0:
+                if a[l]=="1":
+                    if car=="0":
+                        re+="1"
+                    else:
+                        re+="0"
+                        car="1"
+                else:
+                    re+=car
+                    car="0"
+                l-=1
+            while r>=0:
+                if b[r]=="1":
+                    if car=="0":
+                        re+="1"
+                    else:
+                        re+="0"
+                        car="1"
+                else:
+                    re+=car
+                    car="0"
+                r-=1
+            if car=="1":
+                re+="1"
+            return re[::-1]
+        return int(addBinary(bin(a)[2:],bin(b)[2:]),2)
