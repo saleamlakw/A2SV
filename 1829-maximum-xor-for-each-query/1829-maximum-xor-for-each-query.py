ddfb815659f0f7536126788d1ad1ad96bb3c06b8
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-09 22:40:02 +0300

    Time: 1731 ms (5.16%), Space: 34.2 MB (74.73%) - LeetHub

---
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        def bitOn(n,k):
+            return n&(1<<k)!=0
+
+        xor=0
+        for ele in nums:
+            xor^=ele
+        print(xor)
+        result=[]
+        for i in range(len(nums)-1,-1,-1):
+            x=0
+            for j in range(maximumBit):
+                if bitOn(xor,j):
+                    x=x&(~(1<<j))
+                else:
+                    x=x|1<<j           
+            result.append(x)
+            xor^=nums[i]
+        return result
