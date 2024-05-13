93d19f3d3b44e8393d36e3df91e81476ac9168ab
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-13 15:29:52 +0300

    Time: 55 ms (5.95%), Space: 16.6 MB (90.71%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,16 +3,34 @@ class Solution:
         memo=Counter()
         n=len(obstacleGrid)
         m=len(obstacleGrid[0])
-        def inbound(r,c):
-            return 0<=r<n and 0<=c<m
-        def dp(r,c):
-            if not inbound(r,c) or obstacleGrid[r][c]==1:
-                return 0
-            if r==0 and c==0:
-                return 1
-            if (r,c) not in memo:
-                up=dp(r-1,c)
-                left=dp(r,c-1)
-                memo[(r,c)]=up+left
-            return memo[(r,c)]
-        return dp(n-1,m-1)
+
+        dp=[[0]*m for _ in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                if i==0 and j==0 and obstacleGrid[i][j]!=1:
+                    dp[i][j]=1
+                else:
+                    
+                    up,left=0,0
+                    if obstacleGrid[i][j]!=1:
+                        if i-1>=0 and obstacleGrid[i-1][j]!=1:
+                            up=dp[i-1][j]
+                        if j-1>=0 and obstacleGrid[i][j-1]!=1:
+                            left=dp[i][j-1]
+                    dp[i][j]=up+left
+
+        return dp[n-1][m-1]
+        # def inbound(r,c):
+        #     return 0<=r<n and 0<=c<m
+        # def dp(r,c):
+        #     if not inbound(r,c) or obstacleGrid[r][c]==1:
+        #         return 0
+        #     if r==0 and c==0:
+        #         return 1
+        #     if (r,c) not in memo:
+        #         up=dp(r-1,c)
+        #         left=dp(r,c-1)
+        #         memo[(r,c)]=up+left
+        #     return memo[(r,c)]
+        # return dp(n-1,m-1)
