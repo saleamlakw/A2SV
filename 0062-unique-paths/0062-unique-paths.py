9dd12f284b4b6d7a23dde0a995919ba0f0d84139
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-13 14:56:53 +0300

    Time: 41 ms (19.5%), Space: 16.8 MB (11%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,15 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo=Counter()
+        memo=Counter() #--->o(n)+2*o(n)
         def inbound(r,c):
             return 0<=r<m and 0<=c<n
         def dp(r,c):
-            if r==m-1 and c==n-1:
+            if r==0 and c==0:
                 return 1
             if not inbound(r,c):
                 return 0
             if (r,c) not in memo: 
-                memo[(r,c)]= dp(r+1,c)+dp(r,c+1)
+                memo[(r,c)]= dp(r-1,c)+dp(r,c-1)
             return memo[(r,c)]
-        return dp(0,0)
+        return dp(m-1,n-1)
             
