b78359523934c333bfb6033e1d04cae980478c83
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-14 12:42:53 +0300

    Time: 82 ms (5.15%), Space: 25.9 MB (5.44%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,13 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        memo=[[0]*2 for _ in range(len(prices)+1)]
-        for i in range(len(prices)-1,-1,-1):
-            for j in range(2): 
-                profit=0
-                if j:
-                    profit= max(profit,-prices[i]+memo[i+1][0],memo[i+1][1])
+        memo=[[-1]*2 for _ in range(len(prices))]
+        def dp(i,state):
+            if i>=len(prices):
+                return 0
+            if memo[i][state]==-1:
+                profit=float("-inf")
+                if state:
+                    profit= max(profit,-prices[i]+dp(i+1,0),dp(i+1,1))
                 else:
-                    profit= max(profit,prices[i]+memo[i+1][1],memo[i+1][0])
-                memo[i][j]=profit
-        return(memo[0][1])
-        
+                    profit= max(profit,prices[i]+dp(i+1,1),dp(i+1,0))
+                memo[i][state]=profit
+            return memo[i][state]
+        return dp(0,1)
