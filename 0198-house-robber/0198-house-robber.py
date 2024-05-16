d80230a638a91c69e2f1e09cf4e31358c2b0d1a5
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-16 14:31:10 +0300

    Time: 32 ms (79.87%), Space: 16.6 MB (44.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo=Counter()
-        def fn(n):
-            if n==(len(nums)-1):
-                return nums[n]
-            if n>=(len(nums)):
+        memo={}
+        def dp(i):
+            if i>=len(nums):
                 return 0
-            re=float("-inf")
-            for i in range(2,4):
-                if n+i not in memo:
-                    memo[n+i]=fn(n+i)
-                re=max(re,memo[n+i])
-            # print(re,n)
-            return re+nums[n]
-        return max(fn(0),fn(1)) if len(nums)>1 else fn(0)
+            if i not in memo:
+                take=nums[i]+dp(i+2)
+                notake=dp(i+1)
+                memo[i]=(max(take,notake))
+            return memo[i]
+        return dp(0)
        
