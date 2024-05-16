4eaf32f46831d60969d5319ab8f1b16c8f9cc4da
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-16 15:11:29 +0300

    Time: 43 ms (12.08%), Space: 16.6 MB (11.63%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,25 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo={}
+        memo=[[-1,-1] for _ in range(len(nums))]
         def dp(i,taken):
             if i>=len(nums):
                 return 0
-            if (i,taken) not in memo:
+            if memo[i][taken]==-1:
                 if i==0:
                     take=nums[i]+dp(i+2,1)
                     nottake=dp(i+1,0)
-                    memo[(i,taken)]=max(take,nottake)
+                    memo[i][taken]=max(take,nottake)
                 elif i==len(nums)-1:
                     take=float("-inf")
                     if not taken:
                         take=nums[i]+dp(i+2,1)
                     nottake=dp(i+1,0)
-                    memo[(i,taken)]=max(take,nottake)
+                    memo[i][taken]=max(take,nottake)
                 else:
                     take=nums[i]+dp(i+2,taken)
                     nottake=dp(i+1,taken)
-                    memo[(i,taken)]=max(take,nottake)
-            return memo[(i,taken)]
+                    memo[i][taken]=max(take,nottake)
+            return memo[i][taken]
         return dp(0,0)
 
             
