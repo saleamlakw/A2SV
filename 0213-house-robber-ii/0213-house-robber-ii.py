7b9c5d26ed7d9a35de3c44410898979848bb71a8
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-16 15:02:21 +0300

    Time: 31 ms (86.62%), Space: 16.6 MB (11.63%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,25 +1,43 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo={}
-        def dp(i,taken):
-            if i>=len(nums):
-                return 0
-            if (i,taken) not in memo:
+        dp=[[0,0] for _ in range(len(nums)+2)]
+
+        for i in range(len(nums)-1,-1,-1):
+            for taken in range(2):
                 if i==0:
-                    take=nums[i]+dp(i+2,1)
-                    nottake=dp(i+1,0)
-                    memo[(i,taken)]=max(take,nottake)
+                    take=nums[i]+dp[i+2][1]
+                    nottake=dp[i+1][0]
+                    dp[i][taken]=max(take,nottake)
                 elif i==len(nums)-1:
                     take=float("-inf")
                     if not taken:
-                        take=nums[i]+dp(i+2,1)
-                    nottake=dp(i+1,0)
-                    memo[(i,taken)]=max(take,nottake)
+                        take=nums[i]+dp[i+2][1]
+                    nottake=dp[i+1][0]
+                    dp[i][taken]=max(take,nottake)
                 else:
-                    take=nums[i]+dp(i+2,taken)
-                    nottake=dp(i+1,taken)
-                    memo[(i,taken)]=max(take,nottake)
-            return memo[(i,taken)]
-        return dp(0,0)
+                    take=nums[i]+dp[i+2][taken]
+                    nottake=dp[i+1][taken]
+                    dp[i][taken]=max(take,nottake)
+        # memo={}
+        # def dp(i,taken):
+        #     if i>=len(nums):
+        #         return 0
+        #     if (i,taken) not in memo:
+        #         if i==0:
+        #             take=nums[i]+dp(i+2,1)
+        #             nottake=dp(i+1,0)
+        #             memo[(i,taken)]=max(take,nottake)
+        #         elif i==len(nums)-1:
+        #             take=float("-inf")
+        #             if not taken:
+        #                 take=nums[i]+dp(i+2,1)
+        #             nottake=dp(i+1,0)
+        #             memo[(i,taken)]=max(take,nottake)
+        #         else:
+        #             take=nums[i]+dp(i+2,taken)
+        #             nottake=dp(i+1,taken)
+        #             memo[(i,taken)]=max(take,nottake)
+        #     return memo[(i,taken)]
+        return dp[0][0]
 
             
