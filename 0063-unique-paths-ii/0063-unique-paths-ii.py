59fb698d94c2bc2fb96e189e4e1a8bb1295ee298
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-16 19:59:23 +0300

    Time: 50 ms (18.64%), Space: 17 MB (11.1%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,6 +3,20 @@ class Solution:
         memo=Counter()
         n=len(obstacleGrid)
         m=len(obstacleGrid[0])
+        dp=[[0]*m for _ in range(n)]
+        for r in range(n):
+            for c in range(m):
+                if r==0 and c==0 and obstacleGrid[r][c]!=1:
+                    dp[0][0]=1
+                    continue
+                if obstacleGrid[r][c]!=1:
+                    up=dp[r-1][c] if r-1>=0 else 0
+                    left=dp[r][c-1] if c-1 >=0 else 0
+                    dp[r][c]=up+left
+        print(dp)
+        return dp[n-1][m-1]
+
+
         def inbound(r,c):
             return 0<=r<n and 0<=c<m
         def dp(r,c):
