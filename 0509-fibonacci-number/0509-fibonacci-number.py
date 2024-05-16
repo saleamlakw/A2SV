fd3ade5b300b5277878b8941704b90e2ebce027a
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-16 18:24:58 +0300

    Time: 36 ms (66.15%), Space: 16.4 MB (82.95%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -5,7 +5,8 @@ class Solution:
 
             dp=[0]*(n+1)
             dp[0]=0
-            dp[1]=1
+            if n>=1:
+                dp[1]=1
 
             for i in range(2,n+1):
                 dp[i]=dp[i - 1] + dp[i - 2]
