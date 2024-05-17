e002e9cd94359dc7cff655c3a8cf5da5b328fc32
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-17 09:38:48 +0300

    Time: 157 ms (21.56%), Space: 17.6 MB (37.5%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -2,26 +2,44 @@ class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n=len(matrix)
         m=len(matrix[0])
-        def inbound(r,c):
-            return 0<=r<n and 0<=c<m
-        memo=[[float(-inf)]*m for _ in range(n)]
-        def dp(r,c):
-            # print(r,c)
-           
-            if not inbound(r,c):
-                return float("inf")
-            if r==n-1:
-                return matrix[r][c]
-            if memo[r][c]==float(-inf):
+
+
+        dp=[[0]*m for _ in range(n-1)]+[matrix[n-1]]
+        for r in range(n-1,-1,-1):
+            for c in range(m-1,-1,-1):
+                if r==n-1:
+                    continue
                 if r==0:
                     mi=float("inf")
                     for j in range(m):
-                        re=min(matrix[r][j]+dp(r+1,j+1), matrix[r][j]+dp(r+1,j-1),matrix[r][j]+dp(r+1,j))
+                        re=min(matrix[r][j]+dp[r+1][j+1] if r+1<n and j+1<m else float("inf"), matrix[r][j]+dp[r+1][j-1] if r+1<n and j-1>=0 else float("inf"),matrix[r][j]+dp[r+1][j] if r+1<n else float("inf"))
                         mi=min(mi,re)
-                    memo[r][c]= mi
+                    dp[r][c]= mi
                 else:
-                    memo[r][c]=min(matrix[r][c]+dp(r+1,c+1), matrix[r][c]+dp(r+1,c-1),matrix[r][c]+dp(r+1,c))
-            return memo[r][c]
-        return (dp(0,0))
+                    dp[r][c]=min(matrix[r][c]+dp[r+1][c+1] if r+1<n and c+1<m else float("inf"), matrix[r][c]+dp[r+1][c-1] if r+1<n and c-1>=0 else float("inf"),matrix[r][c]+dp[r+1][c] if r+1<n else float("inf"))
+                    # print(dp)
+        return dp[0][0]
+        
+        # def inbound(r,c):
+        #     return 0<=r<n and 0<=c<m
+        # memo=[[float(-inf)]*m for _ in range(n)]
+        # def dp(r,c):
+        #     # print(r,c)
+            
+        #     if not inbound(r,c):
+        #         return float("inf")
+        #     if r==n-1:
+        #         return matrix[r][c]
+        #     if memo[r][c]==float(-inf):
+        #         if r==0:
+        #             mi=float("inf")
+        #             for j in range(m):
+        #                 re=min(matrix[r][j]+dp(r+1,j+1), matrix[r][j]+dp(r+1,j-1),matrix[r][j]+dp(r+1,j))
+        #                 mi=min(mi,re)
+        #             memo[r][c]= mi
+        #         else:
+        #             memo[r][c]=min(matrix[r][c]+dp(r+1,c+1), matrix[r][c]+dp(r+1,c-1),matrix[r][c]+dp(r+1,c))
+        #     return memo[r][c]
+        # return (dp(0,0))
             
             
