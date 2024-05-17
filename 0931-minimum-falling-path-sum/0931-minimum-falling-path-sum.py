ee73d64032fb53d711227065fdb2341b25f73f88
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-17 09:10:19 +0300

    Time: 167 ms (14.04%), Space: 19.1 MB (19.42%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -4,23 +4,24 @@ class Solution:
         m=len(matrix[0])
         def inbound(r,c):
             return 0<=r<n and 0<=c<m
-        memo={}
+        memo=[[float(-inf)]*m for _ in range(n)]
         def dp(r,c):
             # print(r,c)
+           
             if not inbound(r,c):
                 return float("inf")
             if r==n-1:
                 return matrix[r][c]
-            if (r,c)not in memo:
+            if memo[r][c]==float(-inf):
                 if r==0:
                     mi=float("inf")
                     for j in range(m):
                         re=min(matrix[r][j]+dp(r+1,j+1), matrix[r][j]+dp(r+1,j-1),matrix[r][j]+dp(r+1,j))
                         mi=min(mi,re)
-                    memo[(r,c)]= mi
+                    memo[r][c]= mi
                 else:
-                    memo[(r,c)]=min(matrix[r][c]+dp(r+1,c+1), matrix[r][c]+dp(r+1,c-1),matrix[r][c]+dp(r+1,c))
-            return memo[(r,c)]
+                    memo[r][c]=min(matrix[r][c]+dp(r+1,c+1), matrix[r][c]+dp(r+1,c-1),matrix[r][c]+dp(r+1,c))
+            return memo[r][c]
         return (dp(0,0))
             
             
