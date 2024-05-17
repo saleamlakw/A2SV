45ef6ddf2e60e18d4906187994d4d2d0437483b8
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-17 19:29:31 +0300

    Time: 35 ms (61.74%), Space: 20.2 MB (11.83%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,12 +1,17 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        l=0
-        for r in t:
-            if l==len(s):
+        n=len(s)
+        m=len(t)
+        memo=[[-1]*m for _ in range(n)]
+        def dp(i,j):
+            if i>=n:
                 return True
-            if r==s[l]:
-                l+=1
-        return l==len(s) 
-        
-        
-        
+            if j>=m:
+                return False
+            if memo[i][j]==-1:
+                if s[i]==t[j]:
+                    memo[i][j]=dp(i+1,j+1)
+                else:
+                    memo[i][j]=dp(i,j+1)
+            return memo[i][j]
+        return dp(0,0)
