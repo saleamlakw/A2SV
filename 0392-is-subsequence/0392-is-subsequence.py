402759491093cd131befcdded582c537ac29f1a9
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-17 19:29:39 +0300

    Time: 45 ms (6.01%), Space: 17.3 MB (11.83%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -2,16 +2,27 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         n=len(s)
         m=len(t)
-        memo=[[-1]*m for _ in range(n)]
-        def dp(i,j):
-            if i>=n:
-                return True
-            if j>=m:
-                return False
-            if memo[i][j]==-1:
+        dp=[[False]*(m+1) for _ in range(n)]+[[True]*(m+1)]
+        for i in range(n-1,-1,-1):
+            for j in range(m-1,-1,-1):
                 if s[i]==t[j]:
-                    memo[i][j]=dp(i+1,j+1)
+                    dp[i][j]=dp[i+1][j+1]
                 else:
-                    memo[i][j]=dp(i,j+1)
-            return memo[i][j]
-        return dp(0,0)
+                    dp[i][j]=dp[i][j+1]
+        return dp[0][0]
+
+
+
+        # memo=[[-1]*m for _ in range(n)]
+        # def dp(i,j):
+        #     if i>=n:
+        #         return True
+        #     if j>=m:
+        #         return False
+        #     if memo[i][j]==-1:
+        #         if s[i]==t[j]:
+        #             memo[i][j]=dp(i+1,j+1)
+        #         else:
+        #             memo[i][j]=dp(i,j+1)
+        #     return memo[i][j]
+        # return dp(0,0)
