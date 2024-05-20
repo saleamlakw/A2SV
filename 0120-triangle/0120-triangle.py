2b0590fd5731416efe5fb20f0b6bbd74fc8c1f8e
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-20 23:10:36 +0300

    Time: 64 ms (39.04%), Space: 17.9 MB (33.5%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -5,8 +5,6 @@ class Solution:
         dp=[[float("inf")]*(col+1) for _ in range(row-1)]+[triangle[-1]+[float("inf")]]
         for r in range(row-2,-1,-1):
             for c in range(col-1,-1,-1):
-                # if r==(row-1):
-                #     continue
                 if c<len(triangle[r]):
                     dp[r][c]=min(triangle[r][c]+dp[r+1][c],triangle[r][c]+dp[r+1][c+1])
         return dp[0][0]
