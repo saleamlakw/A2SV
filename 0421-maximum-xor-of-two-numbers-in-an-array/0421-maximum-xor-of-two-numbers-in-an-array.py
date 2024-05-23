4453feb81d225c9cf349dd0966e0af90e3e902c9
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-05-23 12:49:58 +0300

    Time: 9193 ms (5.1%), Space: 770.7 MB (5.88%) - LeetHub

---
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.py
@@ -0,0 +1,57 @@
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.isEnd=False
+class Trie:
+    def __init__(self):
+        self.root=TrieNode()
+    def insert(self,word):
+        cur=self.root
+        for letter in word:
+            if letter not in cur.children:
+                 cur.children[letter]=TrieNode() 
+            cur=cur.children.get(letter)
+        cur.isEnd=True
+    def search(self,word):
+        cur=self.root
+        for letter in word:
+            if letter not in cur.children:
+                return False
+            cur=cur.children[letter]
+        return cur.isEnd
+    def startWith(self,prefix):
+        cur=self.root
+        for letter in prefix:
+            if letter not in cur.children:
+                return False
+            cur=cur.children.get(letter)
+        return True
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        trie=Trie()
+        for ele in nums:
+            b=bin(ele)[2:]
+            bi="0"*(32-len(b))+b
+            trie.insert(bi)
+        result=0
+        for num in nums:
+            re=0
+            cur=trie.root.children
+            for i in range(31,-1,-1):
+                if (num & 1<<i):
+                    if "0" in cur:
+                        re=re|1<<i
+                        cur=cur["0"].children
+                    else:
+                        cur=cur["1"].children
+                else:
+                    if "1" in cur:
+                        re=re|1<<i
+                        cur=cur["1"].children
+                    else:
+                        cur=cur["0"].children
+              
+            result=max(result,re)
+        return result
+
+        
