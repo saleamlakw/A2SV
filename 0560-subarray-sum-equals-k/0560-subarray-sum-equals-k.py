544aae5aef418d53dbe41634e9a74fed54fd3694
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-06-02 15:10:12 +0300

    Time: 226 ms (65.7%), Space: 19.4 MB (92.91%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,12 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        dic={0:1}
-        cur=0
-        res=0
-        for i in nums:
-            cur+=i
-            res+=dic.get(cur-k,0)
-            dic[cur]=1+dic.get(cur,0)
-        return res
-                
         
+        hashmap=Counter()
+        hashmap[0]+=1
+
+        accumulator=0
+        result=0
+        for num in nums:
+            accumulator+=num
+            result+=hashmap[accumulator-k]
+            hashmap[accumulator]+=1
+            
+        return result
+
