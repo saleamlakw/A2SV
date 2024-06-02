b2e54e2a3ecfcba4a37ccac5296a0f300d2f91f0
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-06-02 09:30:00 +0300

    Time: 1023 ms (46.31%), Space: 42 MB (38.26%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -1,19 +1,14 @@
-class Solution(object):
-    def shiftingLetters(self, s, shifts):
-        n = len(s)
-        shift_counts = collections.Counter()
-        for start, end, right in shifts:
-            shift_counts[start] += 1 if right else -1 
-            if end + 1 < n:                          
-                shift_counts[end + 1] += -1 if right else 1
-                
-        prefix_sum = [0]                             
-        shifted_string = ''
-        for i in range(n):                         
-            cur_shift = prefix_sum[-1] + shift_counts[i]
-            prefix_sum.append(cur_shift)
-            shifted_char = chr((ord(s[i]) - ord('a') + cur_shift) % 26 + ord('a'))
-            shifted_string += shifted_char
-        return shifted_string
-      
-        
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        arr=[0]*len(s)
+        for l,r,d in shifts:
+            k=2*d-1
+            arr[l]+=k
+            if r+1<len(s):
+                arr[r+1]-=k
+        arr=list(accumulate(arr))
+        result=[]
+        for i in range(len(s)):
+            re=chr((((ord(s[i])+arr[i])-ord("a"))%26)+ord("a"))
+            result.append(re)
+        return "".join(result)
