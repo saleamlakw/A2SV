362585274d65f2302762e5bead8a97eb49a4e886
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-06-10 18:07:44 +0300

    Time: 242 ms (5.08%), Space: 18.5 MB (8.1%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,16 +1,26 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        memo={}
-        def dp(i,t):
-            if i>=len(days):
-                return 0
-            if (i,t) not in memo:
-                re=float("inf")
-                if days[i]>=t:
-                    re=min(dp(i+1,days[i]+1)+costs[0],dp(i+1,days[i]+7)+costs[1],dp(i+1,days[i]+30)+costs[2])
-                else:
-                    re=min(re,dp(i+1,t))
-                memo[(i,t)]=re
-            return memo[(i,t)]
-        return dp(0,0)
-            
+        dp=[[0]*(max(days)+31) for _ in range(len(days)+1)]
+        for i in range(len(days)-1,-1,-1):
+            for pass_days in range(max(days),-1,-1):
+                if days[i]>pass_days:
+                    take=min(costs[0]+dp[i+1][days[i]],costs[1]+dp[i+1][days[i]+6],costs[2]+dp[i+1][days[i]+29])
+                    dp[i][pass_days]=take
+                else:   
+                    nottake=dp[i+1][pass_days]
+                    dp[i][pass_days]=nottake
+        return dp[0][0]
+
+        # memo=Counter()
+        # def dp(i,pass_days):
+        #     if i>=len(days):
+        #         return 0
+        #     if (i,pass_days) not in memo:
+        #         if days[i]>pass_days:
+        #             take=min(costs[0]+dp(i+1,days[i]),costs[1]+dp(i+1,days[i]+6),costs[2]+dp(i+1,days[i]+29))
+        #             memo[(i,pass_days)]=take
+        #         else:   
+        #             nottake=dp(i+1,pass_days)
+        #             memo[(i,pass_days)]=nottake
+        #     return memo[(i,pass_days)]
+        # return dp(0,0)
