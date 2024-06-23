eec53330a35cf00c48481f6beb859419f3ce617c
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-06-23 08:40:08 +0300

    Time: 126 ms (27.07%), Space: 14.8 MB (6.59%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,24 +1,19 @@
-class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-        prefix=[]
-        accumulator=0
-        for num in nums:
-            accumulator+=num
-            prefix.append(accumulator)
-        # print(prefix)
-        postfix=deque()
-        accumulator=0
-        for num in nums[::-1]:
-            accumulator+=num
-            postfix.appendleft(accumulator)
-        # print(postfix)  
-        for n in range(len(prefix)):
-            if prefix[n]==postfix[n]:
-                return n
-        return -1
-        
-"""
-i will calculate postfix sum of the array and prefix sum the array
-i will itertate through both array and find the index in which value in both array is equal
-i will use the index to find the the pivot element 
-"""
+class Solution(object):
+    def pivotIndex(self, nums):
+            prefixSum=[]
+            accumulator=0
+            for r in nums:
+                accumulator+=r
+                prefixSum.append(accumulator)
+            
+            
+            postfixSum=deque()
+            accumulator=0
+            for l in nums[::-1]:
+                accumulator+=l
+                postfixSum.appendleft(accumulator)
+            
+            for i in range(len(prefixSum)):
+                if prefixSum[i]==postfixSum[i]:
+                    return i
+            return -1
