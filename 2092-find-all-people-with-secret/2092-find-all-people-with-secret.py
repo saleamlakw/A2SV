5238ae97bc3c88bf1b06ff4d3a7c54483f1c2b33
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-07-23 20:10:20 +0300

    Time: 1801 ms (20.14%), Space: 84.6 MB (5.19%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,55 +1,50 @@
-# class Dsu:
-#     def __init__(self,n):
-#         self.size=[0]*(n)
-#         self.parent={i:i for i in range(n)}
-#     def union(self,x,y):
-#         parentX=self.find(x)
-#         parentY=self.find(y)
-#         if parentX!=parentY:
-#             if self.size[parentX]>self.size[parentY]:
-#                 self.size[parentX]+=self.size[parentY]
-#                 self.parent[parentY]=self.parent[parentX]
-#             else:
-#                 self.size[parentY]+=self.size[parentX]
-#                 self.parent[parentX]=self.parent[parentY]
-#     def find(self,x):
-#         while x!=self.parent[x]:
-#             self.parent[x]=self.parent[self.parent[x]]
-#             x=self.parent[x]
-#         return x
-#     def is_connected(self,x,y):
-#         return self.find(x)==self.find(y)
+class DSU:
+    def __init__(self,n):
+        self.parent={i:i for i in range(n)}
+        self.size={i:0 for i in range(n)}
+    def find(self,x):
+        while x!=self.parent[x]:
+            self.parent[x]=self.parent[self.parent[x]]
+            x=self.parent[x]
+        return x
+    def union(self,x,y):
+        parentX=self.find(x)
+        parentY=self.find(y)
+        if parentX!=parentY:
+            if self.size[parentX]>self.size[parentY]:
+                self.size[parentX]+=self.size[parentY]
+                self.parent[parentY]=parentX
+            else:
+                self.size[parentY]+=self.size[parentX]
+                self.parent[parentX]=parentY
 class Solution:
-    def findAllPeople(self, n, meetings, firstPerson):
-        # dsu=Dsu(n)
-        # dsu.union(0,firstPerson)
-        sec=set([0,firstPerson])
-        meetings.sort(key=lambda x:x[-1])
-        result=set()
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        dsu=DSU(n)
+        dsu.union(0,firstPerson)
         time=set()
-        all=defaultdict(lambda :defaultdict(set))
-        print(all)
+        all=defaultdict(lambda : defaultdict(list))
         for x,y,t in meetings:
-            all[t][x].add(y)
-            all[t][y].add(x)
+            all[t][x].append(y)
+            all[t][y].append(x)
             time.add(t)
-        time=list(time)
-        time.sort()
+       
+        time=sorted(list(time))
+        result=set([0,firstPerson])
+        def dfs(node):
+            visited.add(node)
+            dsu.union(firstPerson,node)
+            result.add(node)
+            for nb in all[tt][node]:
+                if nb not in visited:
+                   dfs(nb)
         
         for tt in time:
-           for node in all[tt]:
-                if len(all[tt][node].intersection(sec))>=1:
-                    sec=all[tt][node].union(sec)
-                    sec.add(node)
+            visited=set()
+            for node in all[tt]:
+                if node in result:
+                    if node not in visited:
+                        visited.add(node)
+                        dfs(node)
 
-
+        return list(result)
             
-            
-            
-           
-            
-        # if 0 not in result:
-        #     result.add(0)
-        # if firstPerson not in result:
-        #     result.add(firstPerson)    
-        return list(sec)
