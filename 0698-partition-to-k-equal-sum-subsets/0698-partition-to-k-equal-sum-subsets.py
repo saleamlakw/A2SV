b2263d84d0c062200330e65a64005e8f0a13f7c5
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-07-28 22:42:19 +0300

    Time: 609 ms (24.48%), Space: 47.6 MB (8.42%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,22 +1,24 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+    
         tot=sum(nums)
         n=len(nums)
         if tot%k!=0:
             return False 
+        
         target=tot//k
         memo={}
         def backtrack(re,mask):
             if mask==(1<<n)-1:
                 return True
-            if k==target:
+            if re==target:
                 return backtrack(0,mask)
             if (re,mask) not in memo:
                 temp=False
                 for j in range(n):
-                    if mask&(1<<j) and re+nums[j]>target:
+                    if mask&(1<<j) or  re+nums[j]>target:
                         continue
-                    temp =temp or backtrack(re+nums[j],mask|(1<<j))
+                    temp=temp or backtrack(re+nums[j],mask|(1<<j))
                 memo[(re,mask)]=temp
             return memo[(re,mask)]
         return backtrack(0,0)
