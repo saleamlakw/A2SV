9f0d19e6bb198b0c643e38999d0fcba7daf299a5
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-08-17 23:40:16 +0300

    Time: 47 ms (7.95%), Space: 16.6 MB (28.41%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,12 +1,16 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        dp={1:1}
-        def dfs(num):
-            if num in dp:
-                return dp[num]
-            dp[num]=0 if num==n else num
-            for i in range(1,num):
-                val=dfs(i)*dfs(num-i)
-                dp[num]=max(dp[num],val)
-            return dp[num]
-        return dfs(n)
+        memo={}
+        
+        def dp(target):
+            if target==0:
+                return 1
+            if target<0:
+                return float("-inf")
+            if target not in memo:
+                re=float("-inf")
+                for i in range(1,n):
+                    re=max(re,i*dp(target-i))
+                memo[target]=re
+            return memo[target]
+        return dp(n)
