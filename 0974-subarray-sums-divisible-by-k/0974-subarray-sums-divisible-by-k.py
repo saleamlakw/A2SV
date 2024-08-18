20856cbb7a4fcfa54fdea0ad977130d5cf71f933
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-08-19 01:03:50 +0300

    Time: 243 ms (16.76%), Space: 21.4 MB (54.21%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,11 +1,13 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        accumulator=0
         hashmap=Counter()
         hashmap[0]+=1
         result=0
-        tot=0
         for num in nums:
-            tot+=num
-            result+=hashmap[tot%k]
-            hashmap[tot%k]+=1
+            
+            accumulator+=num
+            result+=hashmap[accumulator%k]
+            hashmap[accumulator%k]+=1
+
         return result
