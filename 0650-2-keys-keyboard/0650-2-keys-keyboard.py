14d5e46e7c358c10c9caf58832459c5d7f2021ed
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-08-19 21:29:05 +0300

    Time: 9649 ms (5.01%), Space: 55.2 MB (6.38%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,18 +1,17 @@
 class Solution:
     def minSteps(self, n: int) -> int:
+        
         if n==1:
             return 0
-        memo={}
-        def dp(op,w,v):
-            print(op,w,v)
-            if w==n:
-                return 0
-            if w>n:
-                return float("inf")
-            if (op,w,v) not in memo:
-                paste=1+dp(1,w+v,v)
-                copy=2+dp(0,w+w,w)
-                memo[(op,w,v)]=min(copy,paste)
-            return memo[(op,w,v)]
-        return 2+dp(1,2,1)
-
+        dp=[[float("inf")]*(n+1) for _ in range(n)]
+        dp+=[[0]*(n+1)]
+        # print(dp)
+        for w in range(n,-1,-1):
+            for v in range(n,-1,-1):
+                if w==n:
+                    continue
+                if dp[w][v]==float("inf"):
+                    paste=dp[w+v][v] if w+v<=n else float("inf")
+                    copy=dp[w+w][w] if w+w<=n else float("inf")
+                    dp[w][v]=min(2+copy,1+paste)
+        return 2+dp[2][1]
