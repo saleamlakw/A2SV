ff6156e4b5f9666dcbe673348ac8d059142d94c9
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-09-10 06:57:34 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -6,12 +6,12 @@ class Solution:
             if nums[mid]==target:
                 return mid
             elif nums[left]>nums[mid]:
-                if nums[mid]<target<=nums[right]:
+                if nums[mid]<target:
                     left = mid+1
                 else:
                     right = mid-1
             else:
-                if nums[left]<=target<nums[mid]:
+                if nums[left]<=target:
                     right = mid-1
                 else:
                     left = mid+1
