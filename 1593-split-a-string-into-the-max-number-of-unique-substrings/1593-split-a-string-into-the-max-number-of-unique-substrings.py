fef1fd8bd2f59aead53280090f1340efe435a75a
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-10-21 13:01:03 +0300

    Time: 703 ms (5.16%), Space: 16.6 MB (55.61%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -5,14 +5,15 @@ class Solution:
         def backtrack(i):
             # print(i,visited,len(s))
             if i ==len(s):
-                if "".join(visited)==s:
+                if  "".join(visited)==s:
                     return 0
                 else:
                     return float("-inf")
             
             ans = float("-inf")
             for j in range(1,16):
-                
+                if i+j>16:
+                    break
                 # print(s[i:i+j])
                 if s[i:i+j] not in visited:
                     visited[s[i:i+j]]=""
