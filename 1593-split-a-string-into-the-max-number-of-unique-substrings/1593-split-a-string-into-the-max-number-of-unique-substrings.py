6ca1691b2f7bb6c4fffb59dc770d2a93785404a4
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-10-21 12:46:58 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxUniqueSplit(self, s: str) -> int:
+        visited = {}
+        memo={}
+        def backtrack(i):
+            # print(i,visited,len(s))
+            if i ==len(s):
+                if "".join(visited)==s:
+                    return 0
+                else:
+                    return float("-inf")
+            
+            ans = float("-inf")
+            for j in range(1,16):
+                
+                # print(s[i:i+j])
+                if s[i:i+j] not in visited:
+                    visited[s[i:i+j]]=""
+                    ans = max(ans,1+backtrack(i+j))
+                    visited.pop(s[i:i+j])
+            return ans
+        return (backtrack(0))
