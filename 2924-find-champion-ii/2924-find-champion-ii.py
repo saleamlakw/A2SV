dc2d82dc558e5ce4b3e6f7a6b25b54998606a3a9
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-11-26 17:43:24 +0300

    Time: 18 ms (22.52%), Space: 18.2 MB (9.65%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.py
+++ 2924-find-champion-ii/2924-find-champion-ii.py
@@ -1,11 +1,9 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        graph=defaultdict(list)
-        
+
         indegree=[0]*n
 
         for u,v in edges:
-            graph[u].append(v)
             indegree[v]+=1
 
         if indegree.count(0)!=1:
