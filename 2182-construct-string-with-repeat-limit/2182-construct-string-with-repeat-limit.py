797160ed8f7034c411651e4850626e2b37b9376a
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2024-12-17 08:58:10 +0300

    Time: 183 ms (87.68%), Space: 19.2 MB (34.27%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -1,43 +1,19 @@
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
-        count = Counter() 
-        co = Counter()
-        for i in range(len(s)):
-            co[ord(s[i])]+=1
-        heap = [(-c,co[c])for c in co]
+        heap = [(-ord(val),count) for val,count in Counter(s).items()]
         heapq.heapify(heap)
 
         ans = []
-        pre = -1
         while heap:
-            
-            val,fre = heapq.heappop(heap)
-            val = -val
-            # print(val,pre)
-            if pre == val:
-                if count[val] < repeatLimit:
-                    ans.append(chr(val))
-                    count[val] += 1
-                    if fre - 1>0:
-                        heapq.heappush(heap,(-val,fre-1))
-                else:
-                    if heap:
-                        poped2,fre2 = heapq.heappop(heap)
-                        poped2 = -poped2
-                        ans.append(chr(poped2))
-                        if fre2 - 1>0:
-                            heapq.heappush(heap,(-poped2,fre2-1))
-                        heapq.heappush(heap,(-val,fre))
-                        count = Counter()
-                        count[poped2]+=1
-                        pre = poped2
 
-            else:
-                # print("--",val)
-                ans.append(chr(val))
-                count = Counter()
-                count[val]+=1
-                if fre - 1>0:
-                    heapq.heappush(heap,(-val,fre-1))
-            pre = val
+            val,fre = heapq.heappop(heap)
+            use = min(repeatLimit,fre)
+            ans.append(chr(-val)*use)
+            
+            if fre > use and heap:
+                nxtVal , nxtFre = heapq.heapreplace(heap,(val,fre - use))
+                ans.append(chr(-nxtVal))
+                if nxtFre > 1:
+                    heapq.heappush(heap,(nxtVal,nxtFre-1))
+
         return "".join(ans)
