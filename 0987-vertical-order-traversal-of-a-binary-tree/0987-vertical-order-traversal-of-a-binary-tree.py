118c1a87b14f58c7e0c85c1b7c715be0d915602a
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2025-10-18 16:39:15 +0300

    Time: 3 ms (20.11%), Space: 18.1 MB (37.33%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -6,23 +6,22 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        result=[]
-        def inorder(node,r,c):
-            if not node:
+        verticalOrder = defaultdict(list)
+        minCol = float("inf")
+        maxCol = float("-inf")
+        def dfs(node,r,c):
+            nonlocal minCol , maxCol
+            if not node :
                 return 
-            inorder(node.left,r+1,c-1)
-            result.append([c,r,node.val])
-            inorder(node.right,r+1,c+1)
-        inorder(root,0,0)
-        result.sort()
-        cc=defaultdict(list)
-        prev=result[0][0]
-        i=0
-        re=[[]]
-        for ele in result:
-            if ele[0]!=prev:
-                re.append([])
-            prev=ele[0]
-            re[-1].append(ele[-1])
-        return re
-        
+            minCol = min(minCol,c)
+            maxCol = max(maxCol,c)
+            verticalOrder[c].append((r,node.val))
+            
+            dfs(node.left , r+1, c-1)
+            dfs(node.right , r+1, c+1)
+        dfs(root,0,0)
+        ans = []
+        for col in range(minCol , maxCol+1):
+            ans.append( [y for x,y in sorted(verticalOrder[col])])
+
+        return ans
