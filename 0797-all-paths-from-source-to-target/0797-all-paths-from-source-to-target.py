f63435948cce0bc9cb80171bff7f9469c450b97e
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2025-10-19 22:55:34 +0300

    Time: 3 ms (93.23%), Space: 19.5 MB (10.75%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,13 +1,15 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        start=0
-        destination=len(graph)-1
-
-        ans=[]
-        def dfs(arr,node):
-            if node==destination:
-                ans.append(arr)
+        all_routes = []
+        route = []
+        start = 0
+        destination = len(graph)-1
+        def dfs(node):
+            route.append(node)
+            if node == destination:
+                all_routes.append(route[:])
             for nb in graph[node]:
-                    dfs(arr+[nb],nb)
-        dfs([start],start)
-        return ans
+                dfs(nb)
+            route.pop()
+        dfs(start)
+        return all_routes
