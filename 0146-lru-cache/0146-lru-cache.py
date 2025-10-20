42718b4f8a852493d13e5b60073bcf230d66f3ba
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2025-10-20 19:40:17 +0300

    Time: 189 ms (12.03%), Space: 78 MB (43.74%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,23 +1,45 @@
+class ListNode:
+    def __init__(self,key,val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None 
+
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity 
-        self.cach = OrderedDict()
-   
+        self.cach = {} 
+        self.head = ListNode(-1,-1)
+        self.tail =  ListNode(-1,-1)
+        self.head.next ,  self.tail.prev = self.tail , self.head
+    def insert(self,node):
+        prev , nxt = self.tail.prev , self.tail
+        prev.next = nxt.prev = node
+        node.next = nxt
+        node.prev = prev
+    def remove(self,node):
+       
+        prev, nxt = node.prev , node.next 
+        prev.next = nxt 
+        nxt.prev = prev
     def get(self, key: int) -> int:
         if key in self.cach:
-            self.cach.move_to_end(key)
-            return self.cach[key]
+            self.remove(self.cach[key])
+            self.insert(self.cach[key])
+            return self.cach[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cach:
-             self.cach.move_to_end(key)
-        else:
-            self.cach[key] = value
+            self.remove(self.cach[key])
+        newNode = ListNode(key,value)
+        self.cach[key] = newNode
+        self.insert(self.cach[key])
 
-        if len(self.cach) > self.capacity:
-            self.cach.popitem(last = False)
-    
+        if len(self.cach)>self.capacity:
+            self.cach.pop(self.head.next.key)
+            self.remove(self.head.next)
             
 
 
