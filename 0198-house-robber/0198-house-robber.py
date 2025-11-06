d44ca747d02b703471ddd3f330ca5971a82465af
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2025-11-06 10:44:01 +0300

    Time: 0 ms (100%), Space: 17.9 MB (12.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,21 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # memo={}
-        # def dp(i):
-        #     if i>=len(nums):
-        #         return 0
-        #     if i not in memo:
-        #         take = nums[i]+dp(i+2)
-        #         nottake = dp(i+1)
-        #         memo[i] = max(take,nottake)
-        #     return memo[i]
-        # return dp(0)
-        dp=[float("-inf")]*(len(nums)+2)
-        dp[len(nums)]=0
-        dp[len(nums)+1]=0
-        for i in range(len(nums)-1,-1,-1):
-            take = nums[i]+dp[i+2]
-            nottake = dp[i+1]
-            dp[i] = max(take,nottake)
-        return dp[0]
+        memo = {}
+        def dp(i):
+            if i >= len(nums):
+                return 0 
+            if i not in memo:
+                take = nums[i] + dp(i+2)
+                nottake = dp(i+1)
+                memo[i] = max(take , nottake)
+            return memo[i]
+        return dp(0)
+            
 
